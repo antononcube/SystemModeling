85b4efe07c13c6be5d2660e4d29ed0702eed1b28
antonocube <antononcube@gmail.com>
2020-03-07 12:46:33 -0500

    Corrected the name.

--- Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModels.m
+++ Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModels.m
@@ -1,5 +1,5 @@
 (*
-    System dynamics interactive interfaces functions Mathematica package
+    Epidemiology models Mathematica package
     Copyright (C) 2020  Anton Antonov
 
     This program is free software: you can redistribute it and/or modify
