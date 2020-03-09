3e83304489643c51ce579c3062c76a4f746ff9df
antonocube <antononcube@gmail.com>
2020-03-09 18:36:11 -0400

    Added name and license.

--- Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModelModifications.m
+++ Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModelModifications.m
@@ -1,5 +1,36 @@
-(* Mathematica Package *)
-(* Created by the Wolfram Language Plugin for IntelliJ, see http://wlplugin.halirutan.de/ *)
+(*
+    Epidemiology models modifications Mathematica package
+    Copyright (C) 2020  Anton Antonov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Written by Anton Antonov,
+    antononcube @ gmail . com,
+    Windermere, Florida, USA.
+*)
+
+(*
+    Mathematica is (C) Copyright 1988-2020 Wolfram Research, Inc.
+
+    Protected by copyright law and international treaties.
+
+    Unauthorized reproduction or distribution subject to severe civil
+    and criminal penalties.
+
+    Mathematica is a registered trademark of Wolfram Research, Inc.
+*)
+
 
 (* :Title: EpidemiologyModelModifications *)
 (* :Context: EpidemiologyModelModifications` *)
