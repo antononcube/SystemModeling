fa9c62b145b1c4939faf88a1f80d157a419d8e8a
antonocube <antononcube@gmail.com>
2020-03-30 18:24:44 -0400

    Implemented the function MakeHextilingGraph.

--- Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModelingSimulationFunctions.m
+++ Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModelingSimulationFunctions.m
@@ -58,6 +58,10 @@ If[Length[DownValues[EpidemiologyModelModifications`GetStockSymbols]] == 0,
   Import["https://raw.githubusercontent.com/antononcube/SystemModeling/master/Projects/Coronavirus-propagation-dynamics/WL/EpidemiologyModelModifications.m"]
 ];
 
+If[Length[DownValues[HextileBins`HextileBins]] == 0,
+  Echo["HextileBins.m", "Importing from GitHub:"];
+  Import["https://raw.githubusercontent.com/antononcube/MathematicaForPrediction/master/Misc/HextileBins.m"]
+];
 
 (**************************************************************)
 (* Package definition                                         *)
@@ -72,11 +76,14 @@ into a list equations to be give to NDSolve.";
 
 ModelNDSolve::usage = "ModelNDSolve[model, {t, maxTime}, opts] simulates the model from 0 to maxTime using NDSolve";
 
+MakeHextilingGraph::usage = "MakeHextilingGraph[ aLonLatValue, cellRadius, opts] \
+makes a hexagonal tiling graph for specified data.";
+
 Begin["`Private`"];
 
 Needs["EpidemiologyModels`"];
 Needs["EpidemiologyModelModifications`"];
-
+Needs["HextileBins`"];
 
 (***********************************************************)
 (* ModelNDSolveEquations                                   *)
@@ -143,6 +150,103 @@ ModelNDSolve[___] :=
     Block[{},
       Message[ModelNDSolve::"nargs"];
       $Failed
+    ];
+
+
+(***********************************************************)
+(* MakeHextilingGraph                                       *)
+(***********************************************************)
+
+Clear[MakeHextilingGraph];
+
+MakeHextilingGraph::"nbm" = "The value of the option \"BinMethod\" is expected to be one of \"HextileBins\" or \"GeoHistogram\".";
+
+MakeHextilingGraph::"mr" = "If the value of the option \"BinMethod\" is `1` then the second argument is expected to be `2`.";
+
+Options[MakeHextilingGraph] :=
+    Join[
+      {"BinMethod" -> "HextileBins", "RemoveLoneCells" -> False},
+      Options[HextileBins],
+      Options[GeoHistogram],
+      Options[NearestNeighborGraph]
+    ];
+
+MakeHextilingGraph[
+  aLonLatPopulation : Association[({_?NumberQ, _?NumberQ} -> _?NumberQ) ..],
+  cellRadius : (_?NumberQ | _Quantity),
+  opts : OptionsPattern[] ] :=
+
+    Block[{binMethod, removeLoneCellsQ, grHist, aPolygonValues, lsCells, aCells,
+      nc, lsDistances, pos, grHexagonCellsNetwork, grHexagonCells},
+
+      binMethod = OptionValue[MakeHextilingGraph, "BinMethod"];
+      removeLoneCellsQ = TrueQ[OptionValue[MakeHextilingGraph, "RemoveLoneCells"]];
+
+      Which[
+        ToLowerCase["HextileBins"] == ToLowerCase[binMethod] && NumberQ[cellRadius],
+        aPolygonValues = HextileBins[aLonLatPopulation, cellRadius, FilterRules[{opts}, Options[HextileBins]]],
+
+        ToLowerCase["HextileBins"] == ToLowerCase[binMethod] && !NumberQ[cellRadius],
+        Message[MakeHextilingGraph::"mr", "\"HextileBins\"", "a number"];
+        Return[$Failed],
+
+        ToLowerCase["GeoHistogram"] == ToLowerCase[binMethod] && QuantityQ[cellRadius],
+        grHist = GeoHistogram[KeyMap[Reverse, aLonLatPopulation], cellRadius, Automatic, FilterRules[{opts}, Options[GeoHistogram]]];
+        aPolygonValues = Association@Cases[grHist[[1]], Tooltip[h_Polygon /; MatrixQ[h[[1]]], pop_ /; NumberQ[pop] && pop > 3] :> h -> pop, \[Infinity]],
+
+        ToLowerCase["GeoHistogram"] == ToLowerCase[binMethod] && !QuantityQ[cellRadius],
+        Message[MakeHextilingGraph::"mr", "\"GeoHistogram\"", "Quantity"];
+        Return[$Failed],
+
+        True,
+        Message[MakeHextilingGraph::"nbm"];
+        Return[$Failed]
+      ];
+
+      (* Make cell objects *)
+
+      lsCells = KeyValueMap[<|"Value" -> #2, "Cell" -> #1, "Center" -> Mean[PolygonCoordinates[#1]]|> &, aPolygonValues];
+      lsCells = SortBy[lsCells, #["Center"] &];
+      aCells = AssociationThread[Range[Length[lsCells]], lsCells];
+      aCells = Association@KeyValueMap[#1 -> Prepend[#2, "ID" -> #1] &, aCells];
+
+      (* Create a function to find the nearest cesll to a given position *)
+
+      nc = Nearest[Values[aCells] -> Keys[aCells], DistanceFunction -> (EuclideanDistance[#1["Center"], #2["Center"]] &)];
+
+      lsDistances = Select[Flatten@DistanceMatrix[Values[#["Center"] & /@ aCells]], # > 0 &];
+
+      (* identify outlier(s) and drop them *)
+      If[removeLoneCellsQ,
+        pos = Select[nc[#, {6, 1.1 * Min[lsDistances] / Cos[\[Pi] / 6.]}] & /@ aCells, Length[#] == 1 &];
+        aCells = KeyDrop[aCells, Keys[pos]];
+      ];
+
+      (* reassign cell ID's *)
+
+      aCells = AssociationThread[Range[Length[aCells]], Values[aCells]];
+      aCells = Association@KeyValueMap[#1 -> Prepend[#2, "ID" -> #1] &, aCells];
+
+      (* Make graph *)
+      grHexagonCellsNetwork =
+          NearestNeighborGraph[
+            Keys[aCells], {7, Min[lsDistances] / Cos[\[Pi] / 6.]},
+            DistanceFunction -> (EuclideanDistance[aCells[#1]["Center"], aCells[#2]["Center"]] &),
+            VertexCoordinates -> KeyValueMap[#1 -> #2["Center"] &, aCells],
+            FilterRules[{opts}, Options[NearestNeighborGraph]]
+          ];
+
+      grHexagonCells =
+          Graph[
+            DirectedEdge @@@
+                Join[
+                  EdgeList[grHexagonCellsNetwork],
+                  Reverse /@ EdgeList[grHexagonCellsNetwork]
+                ],
+            DirectedEdges -> True,
+            VertexCoordinates -> KeyValueMap[#1 -> #2["Center"] &, aCells],
+            FilterRules[{opts}, Options[Graph]]
+          ]
     ];
 
 
