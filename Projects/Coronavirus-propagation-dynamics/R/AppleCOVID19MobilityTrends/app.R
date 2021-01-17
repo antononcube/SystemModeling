ece222578e7dcc7bc17b06967e0edb1d246ef4f2
antonocube <antononcube@gmail.com>
2021-01-17 14:35:21 -0500

    First version

---
+++ Projects/Coronavirus-propagation-dynamics/R/AppleCOVID19MobilityTrends/app.R
@@ -0,0 +1,42 @@
+## Appple mobility trends time series search engine
+## https://covid19.apple.com/mobility
+
+library(shiny)
+library(Matrix)
+library(tidyverse)
+library(ggplot2)
+
+library(purrr)
+library(dplyr)
+library(RcppRoll)
+library(stringi)
+library(stringr)
+
+library(SMRMon)
+library(OutlierIdentifiers)
+library(SparseMatrixRecommender)
+library(SparseMatrixRecommenderInterfacesNoDT)
+
+# Get the TS-SMR object:
+load("tssmrApple.RData")
+
+# Search vectors
+tsSearchVectors <- SparseMatrixRecommender::MakeTimeSeriesSearchVectors( tsMat = tssmrApple$TSMat )
+length(tsSearchVectors)
+
+# Use dates
+tssmrApple$TIBNameToTIBRules <- setNames( as.POSIXct(names(tssmrApple$TIBNameToTIBRules)), names(tssmrApple$TIBNameToTIBRules) )
+
+# Define UI for application that draws a histogram
+ui <- SparseMatrixRecommenderInterfacesNoDT::TSCorrSMRMakeUI(tsSMR = tssmrApple, tsSearchVectors = tsSearchVectors, 
+                                                             initNNs = 8, initNCols = 2, plotOutputHeight = "800px",
+                                                             dashboardTitle = "Apple Mobility Trends Search Engine",
+                                                             noteText = "Apple mobility trends data from https://covid19.apple.com/mobility .", 
+                                                             theme = "flatly")
+
+# Define server logic required to draw a histogram
+server <- SparseMatrixRecommenderInterfacesNoDT::TSCorrSMRMakeServerFunction(tsSMR = tssmrApple, tsSearchVectors = tsSearchVectors, roundDigits = 6)
+
+# Run the application 
+# shiny::runApp( TSCorrSMRCreateSearchInterface( tsSMR = tssmrApple ) )
+shinyApp( ui = ui, server = server)
