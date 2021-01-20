d66faf94db59459b0573f5dc65bec9ed12893e4e
antonocube <antononcube@gmail.com>
2021-01-20 15:42:05 -0500

    Imposing rownames and another reference link.

--- Projects/Coronavirus-propagation-dynamics/R/AppleCOVID19MobilityTrends/app.R
+++ Projects/Coronavirus-propagation-dynamics/R/AppleCOVID19MobilityTrends/app.R
@@ -20,6 +20,9 @@ library(SparseMatrixRecommenderInterfacesNoDT)
 # Get the TS-SMR object:
 load("tssmrApple.RData")
 
+# Make sure there is row names correspondence 
+tssmrApple$TSMat <- SMRImposeRowIDs( smat = tssmrApple$TSMat, rowIDs = rownames(tssmrApple$SMR$M) )
+
 # Search vectors
 tsSearchVectors <- SparseMatrixRecommender::MakeTimeSeriesSearchVectors( tsMat = tssmrApple$TSMat )
 length(tsSearchVectors)
@@ -31,7 +34,10 @@ tssmrApple$TIBNameToTIBRules <- setNames( as.POSIXct(names(tssmrApple$TIBNameToT
 ui <- SparseMatrixRecommenderInterfacesNoDT::TSCorrSMRMakeUI(tsSMR = tssmrApple, tsSearchVectors = tsSearchVectors, 
                                                              initNNs = 8, initNCols = 2, plotOutputHeight = "800px",
                                                              dashboardTitle = "Apple Mobility Trends Search Engine",
-                                                             noteText = "Apple mobility trends data from https://covid19.apple.com/mobility .", 
+                                                             noteText = 
+                                                               paste( "Apple mobility trends data from https://covid19.apple.com/mobility .", 
+                                                                      "Full description here : https://mathematicaforprediction.wordpress.com/2020/04/17/apple-mobility-trends-data-visualization-for-covid-19 .", 
+                                                                      sep = "\n"),
                                                              theme = "flatly")
 
 # Define server logic required to draw a histogram
